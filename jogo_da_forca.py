aba07bc054605de394c5b4a1ffbdbaac8720cc69
PedroGregori <ferreiragregorimelo@gmail.com>
2022-07-14 17:51:09 -0300

    Novas implementações e atualizações

--- jogo_da_forca.py
+++ jogo_da_forca.py
@@ -1,14 +1,22 @@
 import os
+import math
 op = 0
 score = 0
-while op != 3:
+while op != 4:
   def txt():
-    print("::::::::::::\Jogo da Forca/::::::::::::")
+    print(":::::::::::::::\Jogo da Forca/:::::::::::::::")
+  def lin():
+    a = w*1.5
+    b = '='*math.ceil(a)
+    return b
   txt()
-  print('[1] Nova partida\n[2] Ver pontuações\n[3] Sair')
+  print()
+  print('[1] Nova partida\n[2] Pontuações\n[3] Informações do jogo\n[4] Sair')
   op = int(input("O que deseja fazer? --> "))  
+  
   word = '  '
   hidden_word = []
+  l_errada = []
   erro = 0
   acerto = 0
   os.system('cls')
@@ -17,34 +25,70 @@ while op != 3:
     os.system('cls')
     txt() 
     word = input("Digite a palavra: ")
+    word = word.lower()
+    w = len(word)
     dica = input("Dê uma dica: ")
     for letra in word:
       hidden_word.append('*')
-
-    while acerto < len(word):
+      
+    while acerto < w:
         os.system('cls')
         txt()
-        print('Pontuação:',score)
-        print(f'Palavra escondida:\n{hidden_word}')
-        print('Quantidade de letra da palavra:',len(word))
+        print(f'{lin()}|PONTUAÇÃO: {score}|{lin()}')
+        print(f'{lin()}|Palavra escondida|{lin()}\n{hidden_word}')
+        print('----|Quantidade de letra da palavra:',w,'|----')
         print('Dica:',dica)
+        print('Erros:',l_errada)
         l = input("Digite uma letra: ")
+        l = l.lower()
 
         for indice, letra in enumerate(word):
           if l == letra:
-              hidden_word[indice] = l
+              l_caps = l.upper()
+              hidden_word[indice] = l_caps
               acerto += 1
 
-        if l not in word:
-              erro += 1
-              if erro >= 6:
-                print("Você perdeu!")
+        if acerto == w//2:
+          os.system('cls')
+          print('Você acaba de ter chance de acertar a palavra inteira!\n [1] Sim [2] Não')
+          chance = int(input('Deseja tentar?: '))
+          while chance != 2:
+            if chance == 1:
+              c_unica = input("Digite a palvra: ")
+              if c_unica == word:
+                score = (score + 15)-5
+                acerto = w
                 break
-        print('\n')
-        if acerto == len(word):
+              else:
+                erro = 5
+                score -= 5
+                if score < 0:
+                  score = 0
+                break
+
+        if l not in word:
+              l_errada.append(l) 
+              erro += 1
+        if erro >= 5:
+            print("Você perdeu!")
+            break
+        print()
+        
+        if acerto == w:
           score = score + 5
+          if erro >= 1:
+            score -= erro
           print('Você acertou a palavra. Parabéns!')
 
   if op == 2:
     print('Pontuação:',score)
+  if op ==3:
+      os.system('cls')
+      print('----------|Informações sobre o game|-----------')
+      print('Ojetivo: Acertar a palavra escondida')
+      print('Chances: Você tem 5 vidas, caso erre 5 letras\nperderá a partida')
+      print('Chance única: Caso acerte a palavra na \nchance única +15 pontos, se errar -5 pontos')
+      print('Pontuação: Se conseguir acertar a palavra\nsem errar +5 pontos a cada letra errada -1 ponto')
+      print('-'*45)
+
 print("Fim :P")
